9062753d86b724e6a7ed4cd9ef3a013b1fd8e440
Naveen8282 <67773226+Naveen8282@users.noreply.github.com>
2023-09-20 14:52:32 +0530

    first commit
    1

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import { Route,Routes } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Register from './pages/Register'
+import Login from './pages/Login'
+function App() {
+
+  return (
+    <>
+    <div>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element ={<Home/>}/>
+        <Route path='/register' element={<Register/>}/>
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
+    </div>
+    </>
+  )
+}
+
+export default App
